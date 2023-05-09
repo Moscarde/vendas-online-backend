b7bdd8440b5591ac2f19c1b1d7003e40c08eaf4b
Moscarde <theizera@gmail.com>
2023-05-08 23:38:29 -0300

    npx typeorm migration:create

---
+++ src/migration/1683599867947-create_table_user.ts
@@ -0,0 +1,11 @@
+import { MigrationInterface, QueryRunner } from "typeorm"
+
+export class CreateTableUser1683599867947 implements MigrationInterface {
+
+    public async up(queryRunner: QueryRunner): Promise<void> {
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {
+    }
+
+}
